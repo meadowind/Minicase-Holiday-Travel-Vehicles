c9ecdabfad3840d5a4e7f96bcd7ecc5bb9ced09d
meadowind <xchenxxing@gmail.com>
2025-09-22 23:19:06 +0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -11,22 +11,9 @@ public class Customer {
         this.phone = phone;
     }
 
-    // Getters and Setters
-    public String getCustomerID() { return customerID; }
-    public void setCustomerID(String customerID) { this.customerID = customerID; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getAddress() { return address; }
-    public void setAddress(String address) { this.address = address; }
-
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-
     @Override
     public String toString() {
-        return "Customer [ID=" + customerID + ", Name=" + name + 
+        return "Customer [ID=" + customerID + ", Name=" + name +
                ", Address=" + address + ", Phone=" + phone + "]";
     }
 }
