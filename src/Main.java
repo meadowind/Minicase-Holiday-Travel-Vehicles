2f471926f6b388a268124c5a522e2fa8ba061444
meadowind <xchenxxing@gmail.com>
2025-09-22 23:21:26 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ public class Main {
         Customer customer = new Customer("C001", "Alice Smith", "123 Main St", "555-1234");
         Salesperson salesperson = new Salesperson("S001", "Bob Johnson");
 
-        Invoice invoice = new Invoice("INV1001", "2025-09-22", 32000, 2000, 500, customer, salesperson, vehicle);
-        invoice.setTradeInVehicle(new TradeInVehicle("T001", "Toyota", "Corolla", 2018));
-        invoice.addOption(new Option("OPT01", "GPS Navigation", 800));
-        invoice.addOption(new Option("OPT02", "Leather Seats", 1200));
+        Invoice invoice = new Invoice("INV1001", "2025-09-22", 32000, 2000, 500,
+                                      customer, salesperson, vehicle);
 
         System.out.println(invoice);
     }
